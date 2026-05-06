70fb4aea5fc615c87894ebdc67cf2393bd74fb7a
Kaylin98 <kaylinmaharaj@gmail.com>
2026-05-06 19:53:12 +0200

    feat: add player health bar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,14 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int startingHealth = 5;
+    [Range(1, 10)]
+    [SerializeField] int startingHealth = 10;
     [SerializeField] CinemachineCamera deathCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
     int currentHealth;
     int deathCameraPriority = 20;
 
@@ -13,19 +16,35 @@ public class PlayerHealth : MonoBehaviour
     void Awake()
     {
         currentHealth = startingHealth;
+        AdjustShieldUI();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
-        Debug.Log("Player took damage, current health: " + currentHealth);
+        AdjustShieldUI();
 
         if (currentHealth <= 0)
         {
             weaponCamera.parent = null;
             deathCamera.Priority = deathCameraPriority;
             Destroy(this.gameObject);
+        }
+    }
+
+    void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].enabled = true;
+            }
+            else
+            {
+                shieldBars[i].enabled = false;
+            }
         }
     }
     
